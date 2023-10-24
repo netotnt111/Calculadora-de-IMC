fc97bccb829864923005f41597c5a7647500a939
netotnt111 <148286462+netotnt111@users.noreply.github.com>
2023-10-24 02:20:53 -0300

    A atualização mais dahora

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function calcularIMC() {
     var altura = document.getElementById("altura").value;
     var peso   = document.getElementById("peso").value;
     altura /= 100;
-    var imc = (peso / (altura * altura)).toFixed(2);
-    document.getElementById("resultadoIMC").innerHTML = "IMC: " + imc;
-  }
+      var imc = (peso / (altura * altura)).toFixed(2);
+      document.getElementById("resultadoIMC").textContent = "IMC: " + imc;
+    }
